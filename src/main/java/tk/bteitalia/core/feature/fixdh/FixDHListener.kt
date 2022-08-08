fc8d534261b2328f6a3c3e9ca10870bfa1949d7f
wtlgo <georgeendo0@gmail.com>
2022-08-08 07:48:09 +0300

    Add join listener

--- src/main/java/tk/bteitalia/core/feature/fixdh/FixDHListener.kt
+++ src/main/java/tk/bteitalia/core/feature/fixdh/FixDHListener.kt
@@ -1,15 +1,19 @@
 package tk.bteitalia.core.feature.fixdh
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import tk.bteitalia.core.BTEItalyCorePlugin
 import tk.bteitalia.core.worldguard.WGRegionEnterEvent
 
-internal class FixDHListener(private val plugin: BTEItalyCorePlugin) : Listener {
+internal class FixDHListener(
+    private val corePlugin: BTEItalyCorePlugin, private val worldGuardPlugin: WorldGuardPlugin
+) : Listener {
     private fun reloadDH() {
-        plugin.server.scheduler.scheduleSyncDelayedTask(plugin, {
-            val console = plugin.server.consoleSender
-            plugin.server.dispatchCommand(console, "plugman reload DecentHolograms")
+        corePlugin.server.scheduler.scheduleSyncDelayedTask(corePlugin, {
+            val console = corePlugin.server.consoleSender
+            corePlugin.server.dispatchCommand(console, "plugman reload DecentHolograms")
         }, 3)
     }
 
@@ -19,5 +23,20 @@ internal class FixDHListener(private val plugin: BTEItalyCorePlugin) : Listener 
         if (!name.contains("newspawn", ignoreCase = true)) return
 
         reloadDH()
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        val regions = worldGuardPlugin.regionContainer.get(event.player.world) ?: return
+        val spawnRegions = regions.regions.values.filter { it.id.contains("newspawn", ignoreCase = true) }
+
+        val location = event.player.location
+
+        for (region in spawnRegions) {
+            if (!region.contains(location.blockX, location.blockY, location.blockZ)) continue
+
+            reloadDH()
+            return
+        }
     }
 }
