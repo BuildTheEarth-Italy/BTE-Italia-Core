23ad18559c46e2cdf2ab5b11d250b622962ffc49
wtlgo <georgeendo0@gmail.com>
2022-08-08 07:32:45 +0300

    Add onWGRegionEnter handler

--- src/main/java/tk/bteitalia/core/feature/fixhd/FixHDListener.kt
+++ src/main/java/tk/bteitalia/core/feature/fixhd/FixHDListener.kt
@@ -1,6 +1,23 @@
 package tk.bteitalia.core.feature.fixhd
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import tk.bteitalia.core.BTEItalyCorePlugin
+import tk.bteitalia.core.worldguard.WGRegionEnterEvent
 
-internal class FixHDListener : Listener {
+internal class FixHDListener(private val plugin: BTEItalyCorePlugin) : Listener {
+    private fun reloadDH() {
+        plugin.server.scheduler.scheduleSyncDelayedTask(plugin, {
+            val console = plugin.server.consoleSender
+            plugin.server.dispatchCommand(console, "plugman reload DecentHolograms")
+        }, 3)
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun onWGRegionEnter(event: WGRegionEnterEvent) {
+        val name = event.region.id
+        if (!name.contains("newspawn", ignoreCase = true)) return
+
+        reloadDH()
+    }
 }
