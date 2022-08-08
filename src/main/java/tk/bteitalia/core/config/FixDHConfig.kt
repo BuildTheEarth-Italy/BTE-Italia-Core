2fdb01d8489415abb44a25d48bc0cc9284ddc948
wtlgo <georgeendo0@gmail.com>
2022-08-08 14:24:27 +0300

    Automatically save config

--- src/main/java/tk/bteitalia/core/config/FixDHConfig.kt
+++ src/main/java/tk/bteitalia/core/config/FixDHConfig.kt
@@ -5,18 +5,29 @@ import dev.dejvokep.boostedyaml.YamlDocument
 internal class FixDHConfig(private val config: YamlDocument) {
     var enabled: Boolean
         get() = config.getBoolean("features.fix-dh.enabled", false)
-        set(value) = config.set("features.fix-dh.enabled", value)
+        set(value) {
+            config.set("features.fix-dh.enabled", value)
+            config.save()
+        }
 
     var delay: Double
         get() = config.getDouble("features.fix-dh.delay", 1.0)
-        set(value) = config.set("features.fix-dh.delay", value)
+        set(value) {
+            config.set("features.fix-dh.delay", value)
+            config.save()
+        }
 
     var executeCommands: List<String>
         get() = config.getStringList("features.fix-dh.execute-commands", emptyList())
-        set(value) = config.set("features.fix-dh.execute-commands", value)
-
+        set(value) {
+            config.set("features.fix-dh.execute-commands", value)
+            config.save()
+        }
 
     var regions: List<String>
         get() = config.getStringList("features.fix-dh.regions", emptyList())
-        set(value) = config.set("features.fix-dh.regions", value)
+        set(value) {
+            config.set("features.fix-dh.regions", value)
+            config.save()
+        }
 }
