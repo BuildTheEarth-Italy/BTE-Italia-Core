385f5334144c9d7c0975316b3ee547052dc2e708
wtlgo <georgeendo0@gmail.com>
2022-08-08 07:24:17 +0300

    Create WorldGuard Region Enter event

--- src/main/java/tk/bteitalia/core/BTEItalyCorePlugin.kt
+++ src/main/java/tk/bteitalia/core/BTEItalyCorePlugin.kt
@@ -4,15 +4,20 @@ import com.sk89q.worldguard.bukkit.WGBukkit
 import org.bukkit.event.HandlerList
 import org.bukkit.plugin.java.JavaPlugin
 import tk.bteitalia.core.feature.fixhd.FixHDListener
+import tk.bteitalia.core.worldguard.WorldGuardEntryHandler
 
 @Suppress("unused")
 class BTEItalyCorePlugin : JavaPlugin() {
     override fun onEnable() {
-        val wg = WGBukkit.getPlugin()
-        if (wg == null) {
+        val worldGuard = WGBukkit.getPlugin()
+        if (worldGuard == null) {
             logger.severe("WorldGuard is not found!!!")
             server.pluginManager.disablePlugin(this)
             return
+        }
+
+        if (!WorldGuardEntryHandler.register(worldGuard, server.pluginManager)) {
+            logger.warning("Unable to register WorldGuard handler!")
         }
 
         val fixHDListener = FixHDListener()
---
+++ src/main/java/tk/bteitalia/core/worldguard/WorldGuardRegionEnterEvent.kt
@@ -0,0 +1,36 @@
+package tk.bteitalia.core.worldguard
+
+import com.sk89q.worldguard.protection.regions.ProtectedRegion
+import org.bukkit.entity.Player
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+@Suppress("unused")
+internal class WorldGuardRegionEnterEvent(
+    val player: Player,
+    val region: ProtectedRegion
+) : Event(), Cancellable {
+    private var cancelled = false
+
+    override fun isCancelled(): Boolean {
+        return cancelled
+    }
+
+    override fun setCancelled(cancel: Boolean) {
+        cancelled = cancel
+    }
+
+    override fun getHandlers(): HandlerList {
+        return pHandlerList
+    }
+
+    companion object {
+        private val pHandlerList = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return pHandlerList
+        }
+    }
+}
