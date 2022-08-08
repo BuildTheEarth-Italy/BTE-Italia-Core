4459a9a827aab106412cd62dedc5467ce1d068ca
wtlgo <georgeendo0@gmail.com>
2022-08-08 05:22:08 +0300

    Initial commit

---
+++ src/main/java/tk/bteitalia/core/BteItalyCorePlugin.java
@@ -0,0 +1,16 @@
+package tk.bteitalia.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BteItalyCorePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
